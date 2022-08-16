03938ea1abf80d649fcbf4a3f6ed5acbdc59bedf
collinkip <collinskliplangat3@gmail.com>
2022-08-16 20:00:42 +0300

    changing order from ready to active

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 import consumers
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
@@ -40,22 +40,31 @@ class Event(HashModel):
 
 
 @app.get('/deliveries/{pk}/status')
-async def __get_status(pk:str):
-    state=redis.get(f'delivery:{pk}')
+async def get_state(pk: str):
+    state = redis.get(f'delivery:{pk}')
 
     if state is not None:
         return json.loads(state)
 
     return {}
 
+
 @app.post('/deliveries/create')
-async def create(request:Request):
-    body =await request.json()
-    delivery=Delivery(budget=body['data']['budget'],notes=body['data']['notes']).save()
-    event =Event(delivery_id=delivery.pk ,type=body['type'],data=json.dumps(body['data'])).save()
-    state=consumers.create_delivery({},event)
-    redis.set(f'delivery:{delivery.pk}',json.dumps(state))
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes']).save()
+    event = Event(delivery_id=delivery.pk, type=body['type'], data=json.dumps(body['data'])).save()
+    state = consumers.create_delivery({}, event)
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
     return state
 
 
-
+@app.post('/event')
+async def dispatch(request: Request):
+    body = await request.json()
+    delivery_id = body['delivery_id']
+    event = Event(delivery_id=delivery_id, type=body['type'], data=json.dumps(body['data'])).save()
+    state = await get_state(delivery_id)
+    new_state = consumers.start_delivery(state, event)
+    redis.set(f'delivery:{delivery_id}', json.dumps(new_state))
+    return new_state
